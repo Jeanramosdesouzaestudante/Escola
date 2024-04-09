71e95859500aa97f56773451656bbf37eb58bc44
Jeanramosdesouzaestudante <110416890+Jeanramosdesouzaestudante@users.noreply.github.com>
2024-04-09 19:19:55 -0300

    Curso do alura modifico do meu jeito

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-04-16T00:00:00");
-const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo4 = new Date("2024-04-09T22:00:00");
+const tempoObjetivo1 = new Date("2022-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-04-06T00:00:00");
+const tempoObjetivo3 = new Date("2023-01-05T00:00:00");
+const tempoObjetivo4 = new Date("2023-01-18T00:00:00");
 
 const tempos = [ tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
@@ -26,11 +26,11 @@ const tempos = [ tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
+    let tempoFinal =  tempoAtual - tempoObjetivo;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24)
+    let dias = Math.floor(horas/24);
     
     segundos %= 60; 
     minutos %= 60;
