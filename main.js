8f9aea109c96773eb77c67938844b0fec49dc446
Jeanramosdesouzaestudante <110416890+Jeanramosdesouzaestudante@users.noreply.github.com>
2024-04-09 18:54:37 -0300

    Curso alura modificado do meu jeito

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo2 = new Date("2024-04-16T00:00:00");
 const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo4 = new Date("2024-04-09T22:00:00");
 
 const tempos = [ tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
